39fa11b7c9f87702dc3e0fdd201e8005670caff7
Guilherme <guifalcao2017@gmail.com>
2024-10-02 14:38:16 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CursosModule } from './cursos/cursos.module';
+import { TesteComponent } from './teste/teste.component';
+import { HeaderComponent } from './teste/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TesteComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
